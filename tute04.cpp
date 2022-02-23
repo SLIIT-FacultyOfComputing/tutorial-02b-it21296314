26d718364623252508af83425aae80adae4324f6
it21296314 <99176493+it21296314@users.noreply.github.com>
2022-02-23 14:18:05 +0000

    all done

--- tute04.cpp
+++ tute04.cpp
@@ -1,20 +1,28 @@
-/*Exercise 4 - Functions
-
-Write a program to calculate the function called nCr which is defined as
-
-nCr = n!/ r!(n−r)!
-Where n! is the factorial of n.
-
-Implement the functions
-
-long Factorial(int no);
-long nCr(int n, int r);
-Do not modify the main function.*/
-
 #include <iostream>
-
 long Factorial(int no);
 long nCr(int n, int r);
+
+long Factorial(int no)
+{
+  long fac;
+  fac = 1;
+
+    for (int r=no; r >= 1; r--) 
+    {
+        fac = fac * r;
+    }
+    return fac;
+}
+
+long nCr(int n, int r)
+{
+  long answer;
+  answer =  Factorial(n) /  (Factorial(r) *  Factorial(n - r));
+  return answer;
+
+}
+
+
 
 int main() {
   int n, r;
